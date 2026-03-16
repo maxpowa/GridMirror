a4a5847bca1c0867f9bb200881c0cd2a6d5a8d44
maxpowa <maxpowa1@gmail.com>
2026-03-15 21:41:10 -0700

    chore: initial commit

---
+++ GridMirror.cs
@@ -0,0 +1,277 @@
+using Sandbox.Definitions;
+using Sandbox.ModAPI;
+using VRage;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.ObjectBuilders;
+using VRageMath;
+
+namespace GridMirror
+{
+    public enum MirrorAxis { X, Y, Z }
+
+    public class MirrorResult
+    {
+        public int BlocksMirrored;
+        public MyObjectBuilder_CubeGrid? MirroredGrid;
+
+        public override string ToString()
+        {
+            return "Blocks mirrored: " + BlocksMirrored;
+        }
+    }
+
+    public static class GridMirror
+    {
+        public static MirrorResult Mirror(IMyCubeGrid grid, MirrorAxis axis)
+        {
+            var result = new MirrorResult();
+
+            var gridBuilder = (MyObjectBuilder_CubeGrid)grid.GetObjectBuilder();
+
+            if (gridBuilder.CubeBlocks == null || gridBuilder.CubeBlocks.Count == 0)
+                return result;
+
+            var originalBlocks = new System.Collections.Generic.List<MyObjectBuilder_CubeBlock>(gridBuilder.CubeBlocks);
+
+            var min = new Vector3I(int.MaxValue, int.MaxValue, int.MaxValue);
+            var max = new Vector3I(int.MinValue, int.MinValue, int.MinValue);
+
+            foreach (var block in originalBlocks)
+            {
+                var pos = (Vector3I)block.Min;
+                min = Vector3I.Min(min, pos);
+                max = Vector3I.Max(max, pos);
+            }
+
+            var centerSum = min + max;
+
+            var mirroredBlocks = new System.Collections.Generic.List<MyObjectBuilder_CubeBlock>();
+            foreach (var block in originalBlocks)
+            {
+                var copy = (MyObjectBuilder_CubeBlock)block.Clone();
+                copy.EntityId = 0;
+
+                var defId = new MyDefinitionId(block.TypeId, block.SubtypeName);
+                MyCubeBlockDefinition? def = null;
+                MyDefinitionManager.Static?.TryGetCubeBlockDefinition(defId, out def);
+
+                MirrorBlockPosition(copy, axis, centerSum, def!);
+                MirrorBlockOrientation(copy, axis, def!);
+
+                mirroredBlocks.Add(copy);
+                result.BlocksMirrored++;
+            }
+
+            gridBuilder.CubeBlocks.Clear();
+            gridBuilder.CubeBlocks.AddRange(mirroredBlocks);
+
+            MyAPIGateway.Entities.RemapObjectBuilder(gridBuilder);
+
+            gridBuilder.PositionAndOrientation = new MyPositionAndOrientation(grid.WorldMatrix);
+
+            result.MirroredGrid = gridBuilder;
+            return result;
+        }
+
+        private static void MirrorBlockPosition(MyObjectBuilder_CubeBlock block, MirrorAxis axis, Vector3I centerSum, MyCubeBlockDefinition def)
+        {
+            var pos = (Vector3I)block.Min;
+
+            if (def != null && (def.Size.X > 1 || def.Size.Y > 1 || def.Size.Z > 1))
+            {
+                var orientation = new MyBlockOrientation(block.BlockOrientation.Forward, block.BlockOrientation.Up);
+                Matrix orientMatrix;
+                orientation.GetMatrix(out orientMatrix);
+
+                var sizeMinusOne = new Vector3(def.Size.X - 1, def.Size.Y - 1, def.Size.Z - 1);
+                var orientedSize = Vector3.Abs(Vector3.TransformNormal(sizeMinusOne, orientMatrix));
+                var extent = new Vector3I(
+                    (int)System.Math.Round(orientedSize.X),
+                    (int)System.Math.Round(orientedSize.Y),
+                    (int)System.Math.Round(orientedSize.Z));
+
+                var blockMax = pos + extent;
+
+                var mirroredMin = MirrorVector(pos, axis, centerSum);
+                var mirroredMax = MirrorVector(blockMax, axis, centerSum);
+
+                block.Min = (VRage.SerializableVector3I)Vector3I.Min(mirroredMin, mirroredMax);
+            }
+            else
+            {
+                block.Min = (VRage.SerializableVector3I)MirrorVector(pos, axis, centerSum);
+            }
+        }
+
+        private static Vector3I MirrorVector(Vector3I v, MirrorAxis axis, Vector3I centerSum)
+        {
+            switch (axis)
+            {
+                case MirrorAxis.X: return new Vector3I(centerSum.X - v.X, v.Y, v.Z);
+                case MirrorAxis.Y: return new Vector3I(v.X, centerSum.Y - v.Y, v.Z);
+                case MirrorAxis.Z: return new Vector3I(v.X, v.Y, centerSum.Z - v.Z);
+                default: return v;
+            }
+        }
+
+        private static void MirrorBlockOrientation(MyObjectBuilder_CubeBlock block, MirrorAxis axis, MyCubeBlockDefinition def)
+        {
+            if (def == null)
+            {
+                var fwd = SimpleMirrorDirection(block.BlockOrientation.Forward, axis);
+                var up = SimpleMirrorDirection(block.BlockOrientation.Up, axis);
+                block.BlockOrientation = new SerializableBlockOrientation(fwd, up);
+                return;
+            }
+
+            MyCubeBlockDefinition mirrorDef = def;
+            if (!string.IsNullOrEmpty(def.MirroringBlock))
+            {
+                var mirrorDefId = new MyDefinitionId(def.Id.TypeId, def.MirroringBlock);
+                MyCubeBlockDefinition? tempDef = null;
+                if (MyDefinitionManager.Static.TryGetCubeBlockDefinition(mirrorDefId, out tempDef) && tempDef != null)
+                {
+                    mirrorDef = tempDef;
+                    block.SubtypeName = mirrorDef.Id.SubtypeName;
+                }
+            }
+
+            var orientation = block.BlockOrientation;
+            var forwardVec = Base6Directions.GetVector(orientation.Forward);
+            var upVec = Base6Directions.GetVector(orientation.Up);
+            Matrix sourceMatrix = Matrix.CreateWorld(Vector3.Zero, forwardVec, upVec);
+
+            Vector3 mirrorNormal = GetMirrorNormal(axis);
+
+            MySymmetryAxisEnum blockMirrorAxis = MySymmetryAxisEnum.None;
+            if (IsAligned(sourceMatrix.Right, mirrorNormal))
+                blockMirrorAxis = MySymmetryAxisEnum.X;
+            else if (IsAligned(sourceMatrix.Up, mirrorNormal))
+                blockMirrorAxis = MySymmetryAxisEnum.Y;
+            else if (IsAligned(sourceMatrix.Forward, mirrorNormal))
+                blockMirrorAxis = MySymmetryAxisEnum.Z;
+
+            MySymmetryAxisEnum symmetryOp = MySymmetryAxisEnum.None;
+            switch (blockMirrorAxis)
+            {
+                case MySymmetryAxisEnum.X: symmetryOp = def.SymmetryX; break;
+                case MySymmetryAxisEnum.Y: symmetryOp = def.SymmetryY; break;
+                case MySymmetryAxisEnum.Z: symmetryOp = def.SymmetryZ; break;
+            }
+
+            Matrix targetMatrix = ApplySymmetryRotation(symmetryOp, sourceMatrix);
+
+            var newForward = Base6Directions.GetClosestDirection(targetMatrix.Forward);
+            var newUp = Base6Directions.GetClosestDirection(targetMatrix.Up);
+            block.BlockOrientation = new SerializableBlockOrientation(newForward, newUp);
+        }
+
+        private static bool IsAligned(Vector3 a, Vector3 b)
+        {
+            return System.Math.Abs(System.Math.Abs(Vector3.Dot(a, b)) - 1.0f) < 0.001f;
+        }
+
+        private static Vector3 GetMirrorNormal(MirrorAxis axis)
+        {
+            switch (axis)
+            {
+                case MirrorAxis.X: return Vector3.Right;
+                case MirrorAxis.Y: return Vector3.Up;
+                default: return Vector3.Forward;
+            }
+        }
+
+        private static Matrix ApplySymmetryRotation(MySymmetryAxisEnum symmetryOp, Matrix source)
+        {
+            float pi = MathHelper.Pi;
+            float halfPi = MathHelper.PiOver2;
+
+            switch (symmetryOp)
+            {
+                case MySymmetryAxisEnum.X:
+                    return Matrix.CreateRotationX(pi) * source;
+                case MySymmetryAxisEnum.Y:
+                case MySymmetryAxisEnum.YThenOffsetX:
+                    return Matrix.CreateRotationY(pi) * source;
+                case MySymmetryAxisEnum.Z:
+                case MySymmetryAxisEnum.ZThenOffsetX:
+                    return Matrix.CreateRotationZ(pi) * source;
+
+                case MySymmetryAxisEnum.HalfX:
+                    return Matrix.CreateRotationX(-halfPi) * source;
+                case MySymmetryAxisEnum.HalfY:
+                    return Matrix.CreateRotationY(-halfPi) * source;
+                case MySymmetryAxisEnum.HalfZ:
+                    return Matrix.CreateRotationZ(-halfPi) * source;
+
+                case MySymmetryAxisEnum.MinusHalfX:
+                    return Matrix.CreateRotationX(halfPi) * source;
+                case MySymmetryAxisEnum.MinusHalfY:
+                    return Matrix.CreateRotationY(halfPi) * source;
+                case MySymmetryAxisEnum.MinusHalfZ:
+                    return Matrix.CreateRotationZ(halfPi) * source;
+
+                case MySymmetryAxisEnum.XHalfY:
+                    return Matrix.CreateRotationY(halfPi) * Matrix.CreateRotationX(pi) * source;
+                case MySymmetryAxisEnum.YHalfY:
+                    return Matrix.CreateRotationY(halfPi) * Matrix.CreateRotationY(pi) * source;
+                case MySymmetryAxisEnum.ZHalfY:
+                    return Matrix.CreateRotationY(halfPi) * Matrix.CreateRotationZ(pi) * source;
+
+                case MySymmetryAxisEnum.XHalfX:
+                    return Matrix.CreateRotationX(-halfPi) * Matrix.CreateRotationX(pi) * source;
+                case MySymmetryAxisEnum.YHalfX:
+                    return Matrix.CreateRotationX(-halfPi) * Matrix.CreateRotationY(pi) * source;
+                case MySymmetryAxisEnum.ZHalfX:
+                    return Matrix.CreateRotationX(-halfPi) * Matrix.CreateRotationZ(pi) * source;
+
+                case MySymmetryAxisEnum.XHalfZ:
+                    return Matrix.CreateRotationZ(-halfPi) * Matrix.CreateRotationX(pi) * source;
+                case MySymmetryAxisEnum.YHalfZ:
+                    return Matrix.CreateRotationZ(-halfPi) * Matrix.CreateRotationY(pi) * source;
+                case MySymmetryAxisEnum.ZHalfZ:
+                    return Matrix.CreateRotationZ(-halfPi) * Matrix.CreateRotationZ(pi) * source;
+
+                case MySymmetryAxisEnum.XMinusHalfZ:
+                    return Matrix.CreateRotationZ(halfPi) * Matrix.CreateRotationX(pi) * source;
+                case MySymmetryAxisEnum.YMinusHalfZ:
+                    return Matrix.CreateRotationZ(halfPi) * Matrix.CreateRotationY(pi) * source;
+                case MySymmetryAxisEnum.ZMinusHalfZ:
+                    return Matrix.CreateRotationZ(halfPi) * Matrix.CreateRotationZ(pi) * source;
+
+                case MySymmetryAxisEnum.XMinusHalfX:
+                    return Matrix.CreateRotationX(halfPi) * Matrix.CreateRotationX(pi) * source;
+                case MySymmetryAxisEnum.YMinusHalfX:
+                    return Matrix.CreateRotationX(halfPi) * Matrix.CreateRotationY(pi) * source;
+                case MySymmetryAxisEnum.ZMinusHalfX:
+                    return Matrix.CreateRotationX(halfPi) * Matrix.CreateRotationZ(pi) * source;
+
+                case MySymmetryAxisEnum.None:
+                default:
+                    return source;
+            }
+        }
+
+        private static Base6Directions.Direction SimpleMirrorDirection(Base6Directions.Direction dir, MirrorAxis axis)
+        {
+            switch (axis)
+            {
+                case MirrorAxis.X:
+                    if (dir == Base6Directions.Direction.Left) return Base6Directions.Direction.Right;
+                    if (dir == Base6Directions.Direction.Right) return Base6Directions.Direction.Left;
+                    return dir;
+                case MirrorAxis.Y:
+                    if (dir == Base6Directions.Direction.Up) return Base6Directions.Direction.Down;
+                    if (dir == Base6Directions.Direction.Down) return Base6Directions.Direction.Up;
+                    return dir;
+                case MirrorAxis.Z:
+                    if (dir == Base6Directions.Direction.Forward) return Base6Directions.Direction.Backward;
+                    if (dir == Base6Directions.Direction.Backward) return Base6Directions.Direction.Forward;
+                    return dir;
+                default:
+                    return dir;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,206 @@
+using Sandbox.Game.SessionComponents.Clipboard;
+using Sandbox.ModAPI;
+using VRage.Game;
+using VRage.Game.ModAPI;
+using VRage.Plugins;
+using VRageMath;
+
+namespace GridMirror;
+
+public class Plugin : IPlugin
+{
+    private const string CmdPrefix = "/mirror";
+    private bool _hooked;
+
+    public void Init(object gameInstance)
+    {
+    }
+
+    public void Update()
+    {
+        if (!_hooked && MyAPIGateway.Utilities != null)
+        {
+            MyAPIGateway.Utilities.MessageEnteredSender += OnMessageEntered;
+            _hooked = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_hooked)
+        {
+            MyAPIGateway.Utilities.MessageEnteredSender -= OnMessageEntered;
+            _hooked = false;
+        }
+    }
+
+    private void OnMessageEntered(ulong sender, string messageText, ref bool sendToOthers)
+    {
+        if (!messageText.StartsWith(CmdPrefix, System.StringComparison.OrdinalIgnoreCase))
+            return;
+
+        sendToOthers = false;
+
+        if (MyAPIGateway.Session == null)
+            return;
+
+        if (!MyAPIGateway.Session.CreativeMode && !MyAPIGateway.Session.HasCreativeRights)
+        {
+            ShowMessage("Creative tools must be enabled to use mirror commands.");
+            return;
+        }
+
+        var parts = messageText.Split(' ');
+
+        if (parts.Length > 1 && parts[1].Equals("help", System.StringComparison.OrdinalIgnoreCase))
+        {
+            ShowHelp();
+            return;
+        }
+
+        var axis = MirrorAxis.X;
+
+        if (parts.Length > 1)
+        {
+            switch (parts[1].ToUpperInvariant())
+            {
+                case "X": axis = MirrorAxis.X; break;
+                case "Y": axis = MirrorAxis.Y; break;
+                case "Z": axis = MirrorAxis.Z; break;
+                default:
+                    ShowMessage("Unknown axis: " + parts[1] + ". Use X, Y, or Z.");
+                    return;
+            }
+        }
+
+        ExecuteMirror(axis);
+    }
+
+    private static void ExecuteMirror(MirrorAxis axis)
+    {
+        var grid = GetTargetGrid();
+        if (grid == null)
+        {
+            ShowMessage("No grid found. Look at a grid.");
+            return;
+        }
+
+        var player = MyAPIGateway.Session?.Player;
+        if (player == null)
+            return;
+
+        if (!CanCopyGrid(grid, player))
+        {
+            ShowMessage("You don't have permission to copy this grid.");
+            return;
+        }
+
+        var result = GridMirror.Mirror(grid, axis);
+
+        if (result.MirroredGrid == null)
+        {
+            ShowMessage("Mirror failed — grid has no blocks.");
+            return;
+        }
+
+        var clipComp = MyClipboardComponent.Static;
+        if (clipComp?.Clipboard == null)
+        {
+            ShowMessage("Clipboard unavailable.");
+            return;
+        }
+
+        clipComp.Clipboard.SetGridFromBuilders(
+            new MyObjectBuilder_CubeGrid[] { result.MirroredGrid },
+            Vector3.Zero,
+            0f,
+            false);
+        clipComp.Clipboard.Activate(null);
+
+        ShowMessage("Axis: " + axis + ". " + result + ". Grid copied to clipboard — paste with Ctrl+V.");
+    }
+
+    private static bool CanCopyGrid(IMyCubeGrid grid, IMyPlayer player)
+    {
+        var playerId = player.IdentityId;
+
+        if (player.PromoteLevel >= MyPromoteLevel.Admin)
+            return true;
+
+        var blocks = new System.Collections.Generic.List<IMySlimBlock>();
+        grid.GetBlocks(blocks);
+
+        if (blocks.Count == 0)
+            return true;
+
+        var playerFaction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(playerId);
+        int ownedOrShared = 0;
+
+        foreach (var slim in blocks)
+        {
+            if (slim.FatBlock == null)
+            {
+                ownedOrShared++;
+                continue;
+            }
+
+            long ownerId = slim.FatBlock.OwnerId;
+
+            if (ownerId == 0 || ownerId == playerId)
+            {
+                ownedOrShared++;
+                continue;
+            }
+
+            if (playerFaction != null)
+            {
+                var ownerFaction = MyAPIGateway.Session.Factions.TryGetPlayerFaction(ownerId);
+                if (ownerFaction != null && playerFaction.FactionId == ownerFaction.FactionId)
+                {
+                    ownedOrShared++;
+                    continue;
+                }
+            }
+
+            var relation = slim.FatBlock.GetUserRelationToOwner(playerId);
+            if (relation != VRage.Game.MyRelationsBetweenPlayerAndBlock.Enemies)
+            {
+                ownedOrShared++;
+            }
+        }
+
+        return ownedOrShared > blocks.Count / 2;
+    }
+
+    private static IMyCubeGrid GetTargetGrid()
+    {
+        var camera = MyAPIGateway.Session?.Camera;
+        if (camera != null)
+        {
+            var from = camera.WorldMatrix.Translation;
+            var to = from + camera.WorldMatrix.Forward * 200;
+
+            IHitInfo hit;
+            if (MyAPIGateway.Physics.CastRay(from, to, out hit))
+            {
+                if (hit.HitEntity is IMyCubeGrid hitGrid)
+                    return hitGrid;
+            }
+        }
+
+        return null!;
+    }
+
+    private static void ShowMessage(string text)
+    {
+        MyAPIGateway.Utilities.ShowMessage("Mirror", text);
+    }
+
+    private static void ShowHelp()
+    {
+        ShowMessage("Usage: /mirror [X|Y|Z]");
+        ShowMessage("  X = left/right, Y = up/down, Z = forward/back");
+        ShowMessage("  Look at the target grid.");
+        ShowMessage("  Mirrored grid is copied to clipboard for pasting.");
+    }
+}
